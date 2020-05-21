912cacd71ca590af216259ae78a4cef6cfb23ffd
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2020-05-21 23:19:57 +0700

    Updated Course and Student classes.

---
+++ src/main/java/th/ac/mahidol/ict/final22019/Course.java
@@ -0,0 +1,11 @@
+package th.ac.mahidol.ict.final22019;
+
+public class Course {
+    String id;
+    String name;
+
+    public Course(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
--- src/main/java/th/ac/mahidol/ict/final22019/Student.java
+++ src/main/java/th/ac/mahidol/ict/final22019/Student.java
@@ -1,4 +1,12 @@
 package th.ac.mahidol.ict.final22019;
 
-public class Student {
+import java.util.List;
+
+public class Student extends Person {
+    int studentID;
+
+    public Student(int studentID, String name) {
+        this.studentID = studentID;
+        this.name = name;
+    }
 }
