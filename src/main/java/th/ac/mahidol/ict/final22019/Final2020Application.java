38f853128cbdec8bf5ea1dea4229cc4df8a499fe
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2021-05-25 12:30:19 +0700

    Fixed some small issues

--- src/main/java/th/ac/mahidol/ict/final22019/Final2020Application.java
+++ src/main/java/th/ac/mahidol/ict/final22019/Final2020Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Final22019Application {
+public class Final2020Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Final22019Application.class, args);
+        SpringApplication.run(Final2020Application.class, args);
     }
 
 }
